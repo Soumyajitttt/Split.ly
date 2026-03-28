0b967b167daa8e0a3d6e57d6de29947b1413b79f
Soumyajitttt <soumyajitchaudhury4@gmail.com>
2026-03-29 00:46:55 +0530

    Add methods for token generation and password comparison in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -26,5 +26,26 @@ const userSchema = new Schema({
 }, { timestamps: true });
 
 //task - add method to generate access token and method to compare password
+userSchema.methods.generateAccessToken = async function() {
+    const user = this;
+    const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return token;
+}
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    const user = this;
+    return await bcrypt.compare(password, user.password);
+}
+
+//hash password before saving user
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+});
 
 export const User = mongoose.model('User', userSchema);
