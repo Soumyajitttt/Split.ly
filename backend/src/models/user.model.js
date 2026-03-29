49bc05bdd618bf147fb086b1ed44afe65c9e9c3e
Soumyajitttt <soumyajitchaudhury4@gmail.com>
2026-03-29 20:20:45 +0530

    model fullname fix

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -46,7 +46,7 @@ userSchema.methods.generateAccessToken = function(){
         {
             _id: this._id,
             email: this.email,
-            fullName: this.fullName
+            fullname: this.fullname
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
