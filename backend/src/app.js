9bb668bbb6834c941bf4fbab39c424dba07fb495
Soumyajitttt <soumyajitchaudhury4@gmail.com>
2026-03-31 00:58:20 +0530

    Implement token generation for user authentication and update login response to include refresh token

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from "cookie-parser";
+
 
 const app= express();
 
@@ -9,6 +11,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cookieParser());
 
 //routes
 import userRoutes from "./routes/user.routes.js";
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,26 @@
 import {User} from "../models/user.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
+
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        const accessToken = await user.generateAccessToken();
+        const refreshToken = await user.generateRefreshToken();
+        user.refreshToken = refreshToken;
+
+        await user.save({ validateBeforeSave: false });
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new Error("Error generating tokens: " + error.message);
+    }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
 
     const { fullname, password } = req.body;
@@ -63,14 +83,24 @@ const loginUser = asyncHandler(async (req, res) => {
         return res.status(400).json({ success: false, message: "Invalid email or password" });
     }
 
-    //generate access token
-    const accessToken = await registeredUser.generateAccessToken();
+    //generate access token and refresh token
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(registeredUser._id)
+
 
     //remove password from response
     //registeredUser.password = undefined;
     const user = await User.findById(registeredUser._id).select("-password -refreshToken");
 
-    res.status(200).json({ success: true, message: "User logged in successfully", user, accessToken });
+    //res.status(200).json({ success: true, message: "User logged in successfully", user, accessToken, refreshToken });
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("refreshToken", refreshToken, options)
+    .json({ success: true, message: "User logged in successfully", user, accessToken });
 });
 
 //task
