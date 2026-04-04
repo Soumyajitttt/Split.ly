a48a94cdb09b562fd62a82545e53699188d5f1c1
Soumyajitttt <soumyajitchaudhury4@gmail.com>
2026-04-04 13:56:47 +0530

    Add getExpensesForUser route to expense routes

--- backend/src/routes/expense.routes.js
+++ backend/src/routes/expense.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createExpense, getExpensesForGroup } from "../controllers/expense.controller.js";
+import { createExpense, getExpensesForGroup, getExpensesForUser } from "../controllers/expense.controller.js";
 import authenticateUser from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/:groupId/create-expense", authenticateUser, createExpense);
 router.get("/:groupId/expenses", authenticateUser, getExpensesForGroup);
+router.get("/my-expenses", authenticateUser, getExpensesForUser);
+router.delete("/:expenseId", authenticateUser, deleteExpense);
 
 export default router;
